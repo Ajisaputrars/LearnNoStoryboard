a666a15098e3cf4060bdb294e6d3745bd34be3b4
Aji Saputra Raka Siwi <ajisaputrars@hotmail.com>
2020-08-05 14:13:46 +0700

    Initial Commit

---
+++ LearningNoStoryboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LearningNoStoryboard
+//
+//  Created by Aji Saputra Raka Siwi on 05/08/20.
+//  Copyright © 2020 Aji Saputra Raka Siwi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
